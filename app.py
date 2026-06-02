9ee016bf745d1b6cb62a9f4f7053106a3fea2932
ligureos96-pixel <ligureos96@gmail.com>
2026-06-02 16:52:57 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import Chroma
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_core.documents import Document
-from langchain.chains import create_retrieval_chain
+from langchain import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
