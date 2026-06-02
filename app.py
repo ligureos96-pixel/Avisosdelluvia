8e4680a85c5746bfbbd9318e683460310d93cff7
ligureos96-pixel <ligureos96@gmail.com>
2026-06-02 17:33:17 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 import streamlit as st
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_community.vectorstores import FAISS
-from langchain_core.documents import Document
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
-from langchain_core.embeddings import FakeEmbeddings
 
 if "GOOGLE_API_KEY" in st.secrets:
     os.environ["GOOGLE_API_KEY"] = st.secrets["GOOGLE_API_KEY"]
